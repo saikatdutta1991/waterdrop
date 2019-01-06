2999357b205daecedf2ba1f51b6e0f7afd00887f
saikat <saikatdutta1991@gmail.com>
2019-01-06 22:04:05 +0530

    cors origin issue fixe

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ app.set('view engine', 'pug');
 /** view engine setup end */
 
 /** cross domain api call */
-app.use(function (req, res, next) {
+app.use(function (req, res, next) {   
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
   next();
 });
 /** cross domain api call end*/
